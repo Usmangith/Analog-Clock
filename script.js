07cb45842a0468d056fc0713624537170ef26bde
Usmangith <usmangujjart096451@gmail.com>
2024-03-30 10:49:05 -0700

    create analog clock

---
+++ script.js
@@ -0,0 +1,15 @@
+let hour = document.querySelector("#hour");
+let minutes = document.querySelector("#minutes");
+let seconds = document.querySelector("#seconds");
+setInterval(() => {
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+    let hrotation = 30 * htime + mtime / 2;
+    let mrotation = 6 * mtime;
+    let srotation = 6 * stime;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minutes.style.transform = `rotate(${mrotation}deg)`;
+    seconds.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
